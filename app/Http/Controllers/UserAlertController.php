58a75a6a6ced2fdc2cb66bda503a7164e2a37c12
fidele5 <fideleplk@gmail.com>
2025-05-01 08:28:38 +0200

    Initialisation

---
+++ app/Http/Controllers/UserAlertController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserAlert;
+use Illuminate\Http\Request;
+
+class UserAlertController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserAlert $userAlert)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserAlert $userAlert)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UserAlert $userAlert)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserAlert $userAlert)
+    {
+        //
+    }
+}
